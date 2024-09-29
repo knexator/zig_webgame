9d76a8bd8f17d4fe5dc58072f9977150ee29c008
knexator <daniel.hurtadoramirez314@gmail.com>
2024-09-29 23:39:04 +0200

    bug happens

--- build.zig
+++ build.zig
@@ -9,8 +9,8 @@ pub fn build(b: *std.Build) void {
         .os_tag = .freestanding,
     });
 
-    // const optimize: std.builtin.OptimizeMode = .ReleaseSmall;
-    const optimize: std.builtin.OptimizeMode = .Debug;
+    const optimize: std.builtin.OptimizeMode = .ReleaseSmall;
+    // const optimize: std.builtin.OptimizeMode = .Debug;
 
     const exe = b.addExecutable(.{
         .name = "webgame_v0",
@@ -29,20 +29,4 @@ pub fn build(b: *std.Build) void {
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
     b.installArtifact(exe);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
