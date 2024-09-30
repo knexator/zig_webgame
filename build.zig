d33859a59c8ecda39ce8bd39cbff4c2aae9cc30d
knexator <daniel.hurtadoramirez314@gmail.com>
2024-09-30 19:33:15 +0200

    simpler build

--- build.zig
+++ build.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 
-// taken from https://github.com/daneelsan/minimal-zig-wasm-canvas/blob/master/build.zig
-
-const number_of_pages = 2;
-
 pub fn build(b: *std.Build) void {
     const target = b.resolveTargetQuery(.{
         .cpu_arch = .wasm32,
@@ -11,44 +7,17 @@ pub fn build(b: *std.Build) void {
     });
 
     const optimize: std.builtin.OptimizeMode = .ReleaseSmall;
-    // const optimize: std.builtin.OptimizeMode = .Debug;
 
     const exe = b.addExecutable(.{
-        .name = "webgame_v0",
+        .name = "main",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    exe.global_base = 6560;
-    exe.entry = .disabled;
-    exe.rdynamic = true;
-    exe.export_memory = true;
-    exe.stack_size = std.wasm.page_size;
-
-    exe.initial_memory = std.wasm.page_size * number_of_pages;
-    exe.max_memory = std.wasm.page_size * number_of_pages;
 
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run");
     run_step.dependOn(&run_exe.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        // .target = b.standardTargetOptions(.{}),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
